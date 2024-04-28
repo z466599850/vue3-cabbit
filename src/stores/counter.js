f05bd395568d70e40a4f5b494afb54b2af7e4e92
ling <466599850@qq.com>
2024-04-28 20:42:33 +0800

    测试

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -6,6 +6,7 @@ export const useCounterStore = defineStore('counter', () => {
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
+    1
   }
 
   return { count, doubleCount, increment }
